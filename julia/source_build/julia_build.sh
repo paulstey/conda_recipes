d6ea44851dbb2e31b41eb58226e18fa8ee971445
paulstey <paulstey@gmail.com>
2017-05-04 10:54:23 -0400

    fixed wget

--- julia/source_build/julia_build.sh
+++ julia/source_build/julia_build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo yum install -y m4 patch cmake libgfortran git
+sudo yum install -y
+    m4 \
+    patch \
+    cmake \
+    libgfortran \
+    git
+
 export OPENBLAS_DYNAMIC_ARCH=0
 export OPENBLAS_NO_AVX2=1
 
 cd
 mkdir -p software/build
+cd software/build
 
 cd https://github.com/JuliaLang/julia/releases/download/v0.5.1/julia-0.5.1-full.tar.gz 
 tar -xvf julia-0.5.1-full.tar.gz 
