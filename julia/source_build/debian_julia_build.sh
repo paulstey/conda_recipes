4eb3275ede2fcc227f8a3de3d6b59a9bed79d6d0
paulstey <paulstey@gmail.com>
2017-05-04 11:06:40 -0400

    re-organize for multiple linux distros

---
+++ julia/source_build/debian_julia_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install -y
+    m4 \
+    patch \
+    cmake \
+    gfortran \
+    git
+
+export OPENBLAS_DYNAMIC_ARCH=0
+export OPENBLAS_NO_AVX2=1
+
+cd
+mkdir -p software/build
+cd software/build
+
+cd https://github.com/JuliaLang/julia/releases/download/v0.5.1/julia-0.5.1-full.tar.gz 
+tar -xvf julia-0.5.1-full.tar.gz 
+cd julia-0.5.1
+
+make 
