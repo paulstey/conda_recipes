3d2d70e51cf6c500df916258396e4bf9ce176e34
paulstey <paulstey@gmail.com>
2017-05-22 16:18:22 -0400

    several fixes

--- metamap/build.sh
+++ metamap/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-tar xvjf public_mm_linux_main_2016.tar.bz2
+#tar xvjf public_mm_linux_main_2016v2.tar.bz2
 
-which java > java_path.txt
-sed -i 's/\/bin\/java//' java_path.txt
+which java > $PREFIX/java_path.txt
+sed -i 's/\/bin\/java//' $PREFIX/java_path.txt
 
-export JAVA_HOME=`cat java_path.txt`
+export JAVA_HOME=`cat $PREFIX/java_path.txt`
 export PATH=$PREFIX/public_mm/bin:$PATH
 
-cd public_mm
+cd $PREFIX/public_mm
 
 ./bin/install.sh
