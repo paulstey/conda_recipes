e85a98a56d31b59a84f0cb567ff9e0979aa1abfb
paulstey <paulstey@gmail.com>
2017-05-22 14:37:07 -0400

    minor updates

--- metamap/build.sh
+++ metamap/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-tar -xvjf public_mm_linux_main_2016.tar.bz2
-cd public_mm
+tar xvjf public_mm_linux_main_2016.tar.bz2
 
-export JAVA_HOME=/usr/bin
+which java > java_path.txt
+sed -i 's/\/bin\/java//' java_path.txt
+
+export JAVA_HOME=`cat java_path.txt`
 export PATH=$PREFIX/public_mm/bin:$PATH
+
+cd public_mm
 
 ./bin/install.sh
