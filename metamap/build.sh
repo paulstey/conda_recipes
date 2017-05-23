61efb7fdc303ca386076b885a15fb836bc88b2c4
paulstey <paulstey@gmail.com>
2017-05-23 15:16:01 -0400

    more changes

--- metamap/build.sh
+++ metamap/build.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
-#tar xvjf public_mm_linux_main_2016v2.tar.bz2
+JAVA_BINDIR=dirname `which java`
 
-which java > $PREFIX/java_path.txt
-sed -i 's/\/bin\/java//' $PREFIX/java_path.txt
+JAVA_HOME=dirname $JAVA_BINDIR 
 
-export JAVA_HOME=`cat $PREFIX/java_path.txt`
 export PATH=$PREFIX/public_mm/bin:$PATH
 
 #cd $PREFIX/public_mm
-cd $SRC_DIR
+
+BASEDIR=$PREFIX/share
+cp -R $SRC_DIR  $BASEDIR
+
+export $BASEDIR
+export $JAVA_HOME
+
+cd $BASEDIR
 
 ./bin/install.sh
+
+
+
+
+
+## last version
+#!/bin/bash
+
+#which java > $PREFIX/java_path.txt
+#sed -i 's/\/bin\/java//' $PREFIX/java_path.txt
+
+#export JAVA_HOME=`cat $PREFIX/java_path.txt`
+#export PATH=$PREFIX/public_mm/bin:$PATH
+
+##JAR=$PREFIX/jar
+#SHARE_DIR=$PREFIX/share
+
+# make a libexec directory
+##mkdir -p $JAR
+
+##cd $SRC_DIR
+#cp -R $SRC_DIR $SHARE_DIR
+
+#cd $SHARE_DIR
+
+#./bin/install.sh
