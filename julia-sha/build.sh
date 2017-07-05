59cf7d5c2f797049a004b6f91ae4ad10857ed8cf
paulstey <paulstey@gmail.com>
2017-07-05 14:43:16 -0400

    first commit

---
+++ julia-sha/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+JULIA_VERSION=0.6
+JULIA_PKG_NAME=SHA
+DEST="$PREFIX/lib/julia/packages/v$JULIA_VERSION/$JULIA_PKG_NAME"
+
+mkdir -p "$DEST"
+cp --recursive --archive --no-target-directory "$PWD" "$DEST"
