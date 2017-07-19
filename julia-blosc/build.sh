6261f87a743eb5d932e41a81b0850ae260a2ca46
paulstey <paulstey@gmail.com>
2017-07-19 09:00:17 -0400

    fix issue to successfully build libblosc

--- julia-blosc/build.sh
+++ julia-blosc/build.sh
@@ -7,4 +7,12 @@ DEST="$PREFIX/lib/julia/packages/v$JULIA_VERSION/$JULIA_PKG_NAME"
 mkdir -p "$DEST"
 cp --archive --no-target-directory "$PWD" "$DEST"
 
+# Set up julia package directory so BinDeps is found
+export JULIA_PKGDIR="$PREFIX/lib/julia/packages/"
+julia -e "Pkg.init()"
+
+# Run build script
+cd "$DEST/deps/"
+julia build.jl
+
 julia -e "Pkg.resolve()"
